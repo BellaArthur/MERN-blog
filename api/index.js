5487376c862c8da177f080bd39f80b190198844c
Arthur_Bella <arthurbella898@gmail.com>
2024-03-05 15:23:25 +0100

    create the user mdel

--- api/index.js
+++ api/index.js
@@ -15,5 +15,5 @@ mongoose.connect(process.env.MONGO_URL)
 const app = express();
 
 app.listen(3000, () => {
-    console.log('server is running on port 3000.')
+    console.log('server is running on port 3000')
 });
